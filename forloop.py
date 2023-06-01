11602327247e2e8a1b6e095c4dd56f5d271157e9
jackie <jackie@yahoo.com>
2023-06-01 16:53:18 +0530

    My module source code

---
+++ forloop.py
@@ -0,0 +1,8 @@
+mylist = [2,7,9,4,12,17,12]
+
+for i in mylist:            #value = 1 , 2 ,  3 , 4 
+    if i % 2 == 0:
+        print(i,"is even number")
+    else:
+        print(i,"is odd number")
+
