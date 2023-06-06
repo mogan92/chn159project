6269abb38f0704cdca71e751aea3e680ac4c3870
mogan92 <111417162+mogan92@users.noreply.github.com>
2023-06-06 16:52:54 +0530

    code updated

--- forloop.py
+++ forloop.py
@@ -1,2 +1,10 @@
-for i in range(10):
-    print(i
+mylist = [8,5,12,19,22,24,9]
+evenlist = []
+oddlist = []
+for i in mylist:
+    if i % 2 == 0:
+        evenlist.append(i)
+    else:
+        oddlist.append(i)
+print(evenlist)
+print(oddlist)
